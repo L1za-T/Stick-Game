ccb1fd6a1ace5414af5e40067084ff34694d0d2f
L1za-T <78148912+L1za-T@users.noreply.github.com>
2023-10-13 15:44:30 +0100

    Started adding run animation
    Tried adding run animation. Doesn't work for some reason relating to the controls.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,41 +2,103 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(AudioSource))]
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 10f; 
+    public float speed = 10f;
     public float jumpHeight = 7f; 
-    private Rigidbody2D body;
-    private Animator anim; 
-    private bool grounded; 
+    //public float dashSpeed = 20f;
+    public float crouchHeight = 0.5f;
+    public LayerMask whatIsGround; 
+    public Transform groundCheckPoint; 
+    public float groundCheckRadius = 0.01f;
+
+    public AudioClip jumpSound; 
+    //public AudioClip dashSound; 
+    public AudioClip footstepSound; 
+
+    private Rigidbody2D body; 
+    private Animator animator; 
+    private AudioSource audioSource; 
+    public bool grounded; 
+    public bool canDoubleJump = false;
+    //private bool isDashing = false; 
+    private bool isCrouching = false;
     private bool facingRight = true; 
 
-    private void Awake()
+
+    
+    // Start is called before the first frame update
+    void Awake()
     {
         body = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float horizontalInput =  Input.GetAxisRaw("Horizontal");
-        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
-
-        if(Input.GetKey(KeyCode.Space)&& grounded)
+        grounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
+        
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        body.velocity = new Vector2(horizontalInput*speed, body.velocity.y);
+        animator.SetBool("walk", horizontalInput !=0);
+        
+        if(horizontalInput !=0 && grounded)
         {
-        Jump();
+            PlaySound(footstepSound);
         }
-    }
 
+
+        if(Input.GetKeyDown(KeyCode.Space)&& grounded)
+        {
+            canDoubleJump = true; 
+            Jump();
+             
+        }
+        else if(Input.GetKeyDown(KeyCode.Space)&&canDoubleJump)
+        {
+            Jump();
+            canDoubleJump = false;
+        }
+
+        if((horizontalInput>0&& !facingRight)||(horizontalInput<0&&facingRight))
+        {
+            Flip();
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow)&& grounded)
+        {
+            if(!isCrouching){
+                transform.localScale = new Vector3(transform.localScale.x, crouchHeight, transform.localScale.z);
+                isCrouching = true;
+            }
+             else if (isCrouching)
+        {
+            transform.localScale = new Vector3(transform.localScale.x,1f,transform.localScale.z);
+            isCrouching = false; 
+        }
+        }
+
+        
+    }
+    private void Flip(){
+        Vector3 currentScale = gameObject.transform.localScale; 
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale; 
+        facingRight = !facingRight; 
+    }
     private void Jump(){
         body.velocity = new Vector2(body.velocity.x, jumpHeight);
+        animator.SetTrigger("jump");
         grounded = false;
-    }
-    private void OnCollisionEnter2D(Collision2D other) {
-        if(other.gameObject.CompareTag("Ground"))
-        grounded = true;
+        PlaySound(jumpSound);
     }
 
-
+    private void PlaySound(AudioClip clip)
+    {
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
 }
